87854a197d0aa66f51ed9045eba0639de435694b
stefanos-vlachos <stevenvlaxos@gmail.com>
2025-01-29 05:59:08 +0200

    Implemented audio augmentations that can be performed on GPU

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -1,12 +1,19 @@
 import os
 import sys
+import random
 from typing import Optional
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import matplotlib.pyplot as plt
 import torch
+import torchaudio
 import torchaudio.transforms as T
+import torchaudio.functional as F
+import torch.nn.functional as FC
+
+from torch_audiomentations import Compose, Gain, PolarityInversion, AddColoredNoise, BandPassFilter, BandStopFilter, HighPassFilter, LowPassFilter, PitchShift, Shift, SpliceOut, TimeInversion, PeakNormalization, AddBackgroundNoise
+
 import numpy as np
 import audiomentations as Au
 import json
@@ -169,41 +176,333 @@ class SpecAugment():
             plt.savefig(prefix)
         plt.show()
 
-class WaveAugment():
 
+class WaveAugment():
+    def __init__(self, experiment_config):
+        self.sample_rate = 16000
+        self.params = experiment_config['waveaug_params']
+        self.dataset = experiment_config['dataset_name']
+        self.feature_stats = {
+            "FSD2018": {
+                "avg_centroid": 1944,
+                "avg_bandwidth": 1605,
+                "avg_flatness": 0.056
+            },
+            "nsynth": {
+                "avg_centroid": 1294,
+                "avg_bandwidth": 961,
+                "avg_flatness": 0.224
+            },
+            "ESC-50-master": {
+                "avg_centroid": 1191,
+                "avg_bandwidth": 1669,
+                "avg_flatness": 0.144
+            },
+            "BirdClef": {
+                "avg_centroid": 3038,
+                "avg_bandwidth": 1910,
+                "avg_flatness": 0.127
+            }
+        }
+        self.augmentation_composer = self.setup_augmentation_module()
+
+    def setup_augmentation_module(self):
+        """
+        Prepare the PyTorch audiomentations composer
+        """
+        def get_adapted_snr(type):
+            """
+            Adapt min and max SNR db of added noise based on dataset properties
+
+            Args:
+                type: min or max
+            """
+            min_snr = self.params["min_snr_in_db"]
+            max_snr = self.params["max_snr_in_db"]
+            avg_flatness = self.feature_stats[self.dataset]['avg_flatness']
+            adapted_bound = max_snr * (1 - avg_flatness)
+
+            if type == "min":
+                adapted_min_snr = random.uniform(min_snr, adapted_bound)
+                return adapted_min_snr
+            elif type == "max":
+                adapted_max_snr = random.uniform(adapted_bound, max_snr)
+                return adapted_max_snr
+
+        def get_adapted_lowpass_frequencies(type):
+            """
+            Adapt lower and upper frequency boundaries of low pass filter based on dataset properties
+
+            Args:
+                type: min or max frequency
+            """
+            avg_centroid = self.feature_stats[self.dataset]['avg_centroid']
+            avg_bandwidth = self.feature_stats[self.dataset]['avg_bandwidth']
+
+            if type == "min":
+                return avg_centroid
+            elif type == "max":
+                return avg_centroid + avg_bandwidth / 2
+
+        def get_adapted_highpass_frequencies(type):
+            """
+            Adapt lower and upper frequency boundaries of high pass filter based on dataset properties
+
+            Args:
+                type: min or max frequency
+            """
+            avg_centroid = self.feature_stats[self.dataset]['avg_centroid']
+            avg_bandwidth = self.feature_stats[self.dataset]['avg_bandwidth']
+
+            if type == "min":
+                return avg_centroid - avg_bandwidth / 2
+            elif type == "max":
+                return avg_centroid
+
+        def get_adapted_bandstop_frequencies(type):
+            """
+            Adapt lower and upper frequency boundaries of band stop filter based on dataset properties
+
+            Args:
+                type: min or max frequency
+            """
+            avg_centroid = self.feature_stats[self.dataset]['avg_centroid']
+            avg_bandwidth = self.feature_stats[self.dataset]['avg_bandwidth']
+
+            if type == "min":
+                return avg_centroid - avg_bandwidth / 2
+            elif type == "max":
+                return avg_centroid
+
+        return Compose(
+            transforms = [ 
+                LowPassFilter(
+                    min_cutoff_freq = get_adapted_lowpass_frequencies(type="min"),
+                    max_cutoff_freq = get_adapted_lowpass_frequencies(type="min"),
+                    mode = "per_example",
+                    p = self.params["lowpass_p"],
+                    p_mode = None,
+                    sample_rate  = self.sample_rate, 
+                    target_rate = None,
+                    output_type = 'tensor'
+                ),
+                PitchShift(
+                    min_transpose_semitones = self.params["pitchshift_min_transpose_semitones"],
+                    max_transpose_semitones =self.params["pitchshift_max_transpose_semitones"],
+                    mode = "per_example",
+                    p = self.params["pitchshift_p"],
+                    p_mode = None,
+                    sample_rate = self.sample_rate,
+                    target_rate = None,
+                    output_type = 'tensor'
+                ),
+                Shift(
+                    min_shift = self.params["shift_min_shift"],
+                    max_shift = self.params["shift_max_shift"],
+                    shift_unit = "fraction",
+                    rollover = True,
+                    mode = "per_example",
+                    p = self.params["shift_p"],
+                    p_mode = None,
+                    sample_rate = self.sample_rate,
+                    target_rate = None,
+                    output_type = 'tensor'
+                ),
+                TimeInversion(
+                    mode = "per_example",
+                    p = self.params["timeinversion_p"],
+                    p_mode = None,
+                    sample_rate = self.sample_rate,
+                    target_rate = None,
+                    output_type = 'tensor'
+                ),
+                Gain(
+                    min_gain_in_db = self.params["min_gain_in_db"],
+                    max_gain_in_db = self.params["max_gain_in_db"],
+                    mode = "per_example",
+                    p = self.params["gain_p"],
+                    p_mode = None,
+                    sample_rate = self.sample_rate,
+                    target_rate = None,
+                    output_type = 'tensor'
+                ),
+                AddColoredNoise(
+                    min_snr_in_db = get_adapted_snr(type="min"),
+                    max_snr_in_db = get_adapted_snr(type="max"),
+                    min_f_decay = self.params["noise_min_f_decay"],
+                    max_f_decay = self.params["noise_max_f_decay"],
+                    mode = "per_example",
+                    p = self.params["noise_p"],
+                    p_mode = None,
+                    sample_rate = self.sample_rate,
+                    target_rate = None,
+                    output_type='tensor'
+                ),
+                HighPassFilter(
+                    min_cutoff_freq = get_adapted_highpass_frequencies(type="min"),
+                    max_cutoff_freq = get_adapted_highpass_frequencies(type="max"),
+                    mode = "per_example",
+                    p = self.params["highpass_p"],
+                    p_mode = None,
+                    sample_rate = self.sample_rate,
+                    target_rate = None,
+                    output_type = 'tensor'
+                ),
+                BandStopFilter(
+                    min_center_frequency = get_adapted_bandstop_frequencies(type="min"),
+                    max_center_frequency = get_adapted_bandstop_frequencies(type="max"),
+                    min_bandwidth_fraction = self.params["bandstop_min_bandwidth_fraction"],
+                    max_bandwidth_fraction = self.params["bandstop_max_bandwidth_fraction"],
+                    mode = "per_example",
+                    p = self.params["bandstop_p"],
+                    p_mode = None,
+                    sample_rate = self.sample_rate,
+                    target_rate = None,
+                    output_type='tensor'
+                ),
+                SpliceOut(
+                    num_time_intervals = self.params["spliceout_num_time_intervals"],
+                    max_width = self.params["spliceout_max_width"],
+                    mode = "per_example",
+                    p = self.params["spliceout_p"],
+                    p_mode = None,
+                    sample_rate = self.sample_rate,
+                    target_rate = None,
+                    output_type = 'tensor'
+                )
+            ]
+        )
+
+    def apply_time_masking(self, waveform):
+        """
+        Apply time masking to the given waveform
+
+        Args:
+            waveform: The waveform tensor
+        """
+        waveform_length = waveform.shape[-1]
+
+        # Calculate the number of samples to mask based on the fraction
+        mask_length = int(waveform_length * self.params["timemasking_mask_fraction"])
+
+        # Apply number of masks
+        for _ in range(self.params["timemasking_masks"]):
+            start_idx = random.randint(0, waveform_length - mask_length)
+            waveform[..., start_idx:start_idx + mask_length] = 0
+
+        return waveform
+
+    def apply_time_stretching(self, waveform):
+        """
+        Apply time stretching to the given waveform
+
+        Args:
+            waveform: The waveform tensor
+        """
+        sox_effects = [
+            ["stretch", str(random.uniform(self.params["min_stretch_ratio"], self.params["max_stretch_ratio"]))]
+        ]
+
+        if waveform.ndimension() == 3:
+            waveform = waveform.squeeze(0)
+        waveform, _ = torchaudio.sox_effects.apply_effects_tensor(waveform, 16000, sox_effects)     
+        
+        # Fix length
+        current_length = waveform.shape[-1]  
+        if current_length > 80000:
+            waveform = waveform[..., :80000]  
+        elif current_length < 80000:
+            pad_amount = 80000 - current_length
+            waveform = FC.pad(waveform, (0, pad_amount))    
+
+        return waveform    
+
+    def apply_augmentations(self, waveform):
+        """
+        Apply all augmentations to the given waveform
+
+        Args:
+            waveform: The waveform tensor
+        """
+        augmentations = []
+        waveform_torch = torch.from_numpy(waveform)
+        augmentations.append(waveform_torch)
+        waveform_torch = waveform_torch.unsqueeze(0).unsqueeze(0)
+
+        for i in range(self.params['aug_num']):
+            # Apply pytorch audiomentations
+            augmented_waveform_torch = self.augmentation_composer(waveform_torch, sample_rate=16000)
+
+            # Apply time stretch, if p > 0.5
+            timestretch_p = self.params["timestretch_p"]
+            if random.uniform(0,1) <= timestretch_p:
+                augmented_waveform_torch = self.apply_time_stretching(augmented_waveform_torch)
+
+            # Apply time masking, if p > 0.5
+            timemasking_p = self.params["timemasking_p"]
+            if random.uniform(0,1) <= timemasking_p:
+                augmented_waveform_torch = self.apply_time_masking(augmented_waveform_torch)
+
+            # Resize torch
+            if augmented_waveform_torch.ndimension() == 3:
+                augmented_waveform_torch = augmented_waveform_torch.squeeze(0)
+            
+            # Save augmentation
+            augmentations.append(augmented_waveform_torch.squeeze(0))
+        
+        return augmentations
+
+
+class WaveAugmentOld():
     def __init__(self,experiment_config):
         self.params = experiment_config['waveaug_params']
         self.augmentations = self.define_augmentations()
 
     def define_augmentations(self):
-            augment = Au.Compose([Au.AddGaussianNoise(min_amplitude = self.params['gaussian_min_amplitude'],
-                                                 max_amplitude = self.params['gaussian_max_amplitude'],
-                                                 p = self.params['gaussian_p']),
+            augment = Au.Compose(
+                [
+                    Au.AddGaussianNoise(
+                        min_amplitude = self.params['gaussian_min_amplitude'],
+                        max_amplitude = self.params['gaussian_max_amplitude'],
+                        p = self.params['gaussian_p']
+                    ),
 
-            Au.TimeStretch(min_rate=self.params["timestretch_min_rate"],
-                    max_rate=self.params["timestretch_max_rate"],
-                    p=self.params["timestretch_p"]),
+                    Au.TimeStretch(
+                        min_rate=self.params["timestretch_min_rate"],
+                        max_rate=self.params["timestretch_max_rate"],
+                        p=self.params["timestretch_p"]
+                    ),
         
-            Au.PitchShift(min_semitones=self.params["pitchshift_min_semitones"],
-                    max_semitones=self.params["pitchshift_max_semitones"],
-                    p=self.params["pitchshift_p"]),
+                    Au.PitchShift(
+                        min_semitones=self.params["pitchshift_min_semitones"],
+                        max_semitones=self.params["pitchshift_max_semitones"],
+                        p=self.params["pitchshift_p"]
+                    ),
             
-            Au.Shift(p=self.params["shift_p"]),
+                    Au.Shift(p=self.params["shift_p"]),
 
-            Au.Aliasing(min_sample_rate=self.params["min_sample_rate"],
-                    max_sample_rate=self.params["max_sample_rate"],
-                    p=self.params["aliasing_p"]),
+                    Au.Aliasing(
+                        min_sample_rate=self.params["min_sample_rate"],
+                        max_sample_rate=self.params["max_sample_rate"],
+                        p=self.params["aliasing_p"]
+                    ),
 
-            Au.LowPassFilter(min_cutoff_freq=self.params["min_cutoff_freq"],
-                  max_cutoff_freq=self.params["max_cutoff_freq"],
-                  p=self.params["lowpass_p"]),
+                    Au.LowPassFilter(
+                        min_cutoff_freq=self.params["min_cutoff_freq"],
+                        max_cutoff_freq=self.params["max_cutoff_freq"],
+                        p=self.params["lowpass_p"]
+                    ),
 
-            Au.TimeMask(min_band_part=self.params["min_band_part"],
-                  max_band_part=self.params["max_band_part"],
-                  p=self.params["timemask_p"])])
+                    Au.TimeMask(
+                        min_band_part=self.params["min_band_part"],
+                        max_band_part=self.params["max_band_part"],
+                        p=self.params["timemask_p"]
+                    )
+                ]
+            )
             return augment
 
-    def apply_augmentations(self,waveform):
+    def apply_augmentations(self, waveform):
         augmentations = [torch.from_numpy(waveform)]
         for i in range(self.params['aug_num']):
             augmented_waveform = self.augmentations(samples = waveform, sample_rate = 16000)
@@ -211,7 +510,6 @@ class WaveAugment():
 
         return augmentations
     
-
 
 def preprocessing_and_augmentations(experiment_config, item, test = False):
     read_waveforms = experiment_config['read_waveforms']
@@ -246,25 +544,6 @@ def preprocessing_and_augmentations(experiment_config, item, test = False):
             for wav in augmented_item_list:
                 spec = wav_to_variable_spectrogram(sample = wav, mel_params = mel_params)
                 
-
-                
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 def mel_spec_function(x, sr, n_mels, n_fft, hop_length, power):
         mel_spec_array = librosa.feature.melspectrogram(y=x,
@@ -331,13 +610,12 @@ def wav_to_variable_spectrogram(sample,mel_params):
 
         
 if __name__ == '__main__':
-    with open("experiment_config.json", "r") as f:
+    with open("../config/experiment_config.json", "r") as f:
         experiment_config = json.load(f)
 
     augm_module = WaveAugment(experiment_config = experiment_config)
-    wav = np.load("/data/ESC-50-master/waveforms_npy/dog/1-59513-A-0.npy", allow_pickle = True)
-    print(wav.shape)
-    aug_list = augm_module.apply_augmentations(waveform = wav)
+    wav = np.load("/data/ESC-50-master/waveforms_npy/engine/2-106014-A-44.npy", allow_pickle = True)
+    aug_list = augm_module.apply_augmentations(wav)
     print(len(aug_list))
     print(aug_list[0].shape)
     print(aug_list[1].shape)
@@ -381,18 +659,19 @@ if __name__ == '__main__':
 
     # Save each array in aug_list as a WAV file
     for i, audio_data in enumerate(aug_list):
-        # Ensure audio data is a 1D numpy array
-        if len(audio_data.shape) > 1:
-            audio_data = np.mean(audio_data, axis=1)  # Convert stereo to mono
+        torchaudio.save(f'augmentation_{i}.wav', aug_list[i].unsqueeze(0), sample_rate)
+        # # Ensure audio data is a 1D numpy array
+        # if len(audio_data.shape) > 1:
+        #     audio_data = np.mean(audio_data, axis=1)  # Convert stereo to mono
 
-        # Normalize the audio to avoid clipping
-        max_val = np.max(np.abs(audio_data))
-        if max_val > 1:
-            audio_data = audio_data / max_val
+        # # Normalize the audio to avoid clipping
+        # max_val = np.max(np.abs(audio_data))
+        # if max_val > 1:
+        #     audio_data = audio_data / max_val
 
-        # Save as WAV
-        wav_filename = f"audio_{i + 1}.wav"
-        sf.write(wav_filename, audio_data, samplerate=sample_rate)
-        print(f"Saved: {wav_filename}")
+        # # Save as WAV
+        # wav_filename = f"audio_{i + 1}.wav"
+        # sf.write(wav_filename, audio_data, samplerate=sample_rate)
+        # print(f"Saved: {wav_filename}")
 
 
